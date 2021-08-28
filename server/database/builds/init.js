1da6bead1ac75c045491b12c7d86605c8872f292
Abdallah <dev.aaamra@gmail.com>
2021-08-28 15:32:04 +0300

    create database connection and build files

---
+++ server/database/builds/init.js
@@ -0,0 +1,10 @@
+
+const { join } = require('path');
+const { readFileSync } = require('fs');
+const connection = require('../config/connection');
+
+const sql = readFileSync(join(__dirname, 'init.sql')).toString();
+
+const dbBuild = () => connection.query(sql);
+
+module.exports = dbBuild;
