595044af816d884d844c2fc2a045470ca17e092f
Abdallah <dev.aaamra@gmail.com>
2021-08-28 19:58:31 +0300

    create like post query and improve get posts query

--- server/database/queries/PostsQueries.js
+++ server/database/queries/PostsQueries.js
@@ -1,12 +1,19 @@
 const connection = require('../config/connection');
 
-const getPostsQuery = () => connection('SELECT * FROM posts ORDER BY likes');
+const getPostsQuery = () => connection.query(
+  'SELECT p.title, p.body, p.created_at, u.name, u.username FROM posts p INNER JOIN users u ON u.id = p.user_id ORDER BY p.likes',
+);
 
 const addPostQuery = ({
   title, body, userId, createdAt,
 }) => connection.query(
   'INSERT INTO posts (title, body, user_id, created_at) VALUES ($1, $2, $3, $4) RETURNING *',
   [title, body, userId, createdAt],
+);
+
+const likePostQuery = (postId) => connection.query(
+  'UPDATE posts SET likes = likes + 1 WHERE id = $1',
+  [postId],
 );
 
 const deletePostQuery = (postId) => connection.query(
@@ -18,4 +25,5 @@ module.exports = {
   getPostsQuery,
   addPostQuery,
   deletePostQuery,
+  likePostQuery,
 };
