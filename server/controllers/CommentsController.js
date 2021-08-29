990f2f0028dfebdc58f15e8e115d57eec6ecb4f1
Abdallah <dev.aaamra@gmail.com>
2021-08-29 09:18:38 +0300

    comments controller

---
+++ server/controllers/CommentsController.js
@@ -0,0 +1,43 @@
+const { getCommentsQuery, addCommentQuery, likeCommentQuery } = require('../database/queries/CommentsQueries');
+
+const index = (req, res) => {
+  const { postId } = req.body;
+
+  getCommentsQuery(postId)
+    .then((data) => data.rows)
+    .then((comments) => res.json(comments))
+    // handle error
+    .catch();
+};
+
+const store = (req, res) => {
+  const { body, postId, createdAt } = req.body;
+  // const userId = req.user.id;
+  const userId = 1;
+
+  // validation
+
+  addCommentQuery({
+    body, userId, postId, createdAt,
+  })
+    .then((data) => data.rows[0])
+    .then((comment) => res.json(comment))
+    // handel error
+    .catch();
+};
+
+const like = (req, res) => {
+  const { postId } = req.params;
+
+  likeCommentQuery(postId)
+    .then((data) => data.rows[0])
+    .then((comment) => res.json(comment))
+    // handel error
+    .catch();
+};
+
+module.exports = {
+  index,
+  store,
+  like,
+};
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,10 +1,15 @@
 const router = require('express').Router();
 
 const PostsController = require('../controllers/PostsController');
+const CommentsController = require('../controllers/CommentsController');
 
 router.get('/posts', PostsController.index);
 router.post('/posts', PostsController.store);
 router.put('/posts/:postId/like', PostsController.like);
 router.delete('/posts/:postId', PostsController.destroy);
+
+router.get('/posts/:postId/comments', CommentsController.index);
+router.post('/comments', CommentsController.store);
+router.put('/comments/:postId/like', CommentsController.like);
 
 module.exports = router;
